c464ad608eaef7b5f8a396b6499eb6d00f2b56fa
Djokannn <djordjeradosavljevic14@gmail.com>
2021-01-01 15:58:05 +0100

    first commit

--- SkriptaKomandi.sh
+++ SkriptaKomandi.sh
@@ -1,7 +1,27 @@
 ///////// NASLOV : HTML TAGOVI 
 
 
-ls - blalallalall
+<head></head>-glavni deo sajta npr naslov,opis
+<body></body>-struktura sajta(glavni deo valjda)
+<h1,2,3,4...></h1,2,3,4...>ovo sluzi za naslov a brojevi za velicinu slova
+<hr/>-odvaja linijom tekst 
+<br/>-odvaja tekst
+<big>,<small>..-same ti reci kazu zasta sluzi
+<sub></Sub>-obelezenu rec smanji ali imalo spusi ispod ostalog
+<sup></sup>-obelezenu stavku npr stavi na kvadrat 
+<nav></nav>-nav bar ono na pocetku sajta
+<article></article>-?
+<section></section>-ovo ti je jedno polje(kocka) u sajtu u koju pises povezuje i css sa htmlom takodje
+<a href=""></...>-sluzi za ubacivanje linkova i povezivanje drugih jezika
+<img src=""></..>-sluzi za ubacivanje slika 
+<video></..>-sluzi za ubacivanje videa (ne zaboravi da ti treba sirine debljina
+<iframe>...-sluzi za ubacivanje drugog sajta u tvoj sajt
+<ul>...-lista sa tackama
+<ol>...-lista sa brojevima
+<table>...-tabela a sve ostalo ti pise u prvu vezbu
+<span>...-jedno polje u sajtu
+<div(section)>...-jedna kocka u sajtu zns vec
+
 
 
 
